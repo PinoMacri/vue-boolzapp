aba3c3ecfe6181ba074631b8554d24dee6048d24
PinoMacri <pinonegozio1@outlook.it>
2023-01-13 00:52:13 +0100

    Finish grafica

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
-console.log("ok", Vue)
+const data = {
+    user: {
+      name: 'Nome Utente',
+      avatar: '_io'
+    },
+  }
+  
+  const app = Vue.createApp({
+    data(){
+        return {
+            contacts: [
+                {
+                  name: 'Michele',
+                  avatar: '_1',
+                },
+                {
+                  name: 'Fabio',
+                  avatar: '_2',
+                },
+                {
+                  name: 'Samuele',
+                  avatar: '_3',
+                },
+                {
+                  name: 'Luisa',
+                  avatar: '_4',
+                },
+              ]
+        }
+    }
+  })
+  app.mount("#contatti")
